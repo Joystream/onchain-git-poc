3f14dfe37608c845a0ecaa2773b008452b485715
Arve Knudsen <arve.knudsen@gmail.com>
2019-01-08 11:08:02 +0100

    Fix loading of index

--- tests/test-push.sh
+++ tests/test-push.sh
@@ -3,10 +3,9 @@
 set -eo pipefail
 
 make install
-# gitservicecli tx gitService remove-repo aknudsen/test --chain-id test --from $(gitservicecli keys show aknudsen --address)
 rm -rf /tmp/gitservice && mkdir -p /tmp/gitservice
 cd /tmp/gitservice
 git init sourcerepo && cd sourcerepo
 echo "hello world" > README.md
 git add README.md && git commit -a -m"Start repo"
-gitservicecli tx gitService push-refs aknudsen/test head:refs/heads/master --chain-id test --from $(gitservicecli keys show aknudsen --address)
+gitservicecli tx gitService push-refs aknudsen/test +head:refs/heads/master --chain-id test --from $(gitservicecli keys show aknudsen --address)
