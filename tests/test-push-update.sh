d2f6770a8206d5793b0c53cba908706607fb48d2
Arve Knudsen <arve.knudsen@gmail.com>
2019-01-08 15:32:41 +0100

    Add test

--- tests/test-push-update.sh
+++ tests/test-push-update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Test pushing an initial commit
+# Test pushing a fast-forward update
 set -eo pipefail
 
 make install
@@ -10,3 +10,7 @@ echo "#Hello World" > README.md
 git add README.md && git commit -m"Start repo"
 # Note that we force push to overwrite old content (by prefixing with '+')
 gitservicecli tx gitService push-refs aknudsen/test +head:refs/heads/master --chain-id test --from $(gitservicecli keys show aknudsen --address)
+
+echo "This is a test of pushing branch updates to the blockchain" >> README.md
+git add README.md && git commit -m"Edit README"
+gitservicecli tx gitService push-refs aknudsen/test head:refs/heads/master --chain-id test --from $(gitservicecli keys show aknudsen --address)
